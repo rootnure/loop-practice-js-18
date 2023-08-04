1c5ea2f0e123f11ce5ec5643c20dc26b3616238c
rootnure <nur.diu.2791@gmail.com>
2023-08-04 15:34:33 +0600

    For loop

--- for.js
+++ for.js
@@ -20,15 +20,16 @@ for (var i = 0; i < 7; i++) {
     console.log(i, 'Roast den Please');
 }
  */
-
+/* 
 for (var i = 0; i <= 10; i++) {
     console.log(i);
 }
-
+ */
+/* 
 for (var i = 0; i <= 10; i += 2) {
     console.log(i);
 }
-
+ */
 for (var i = 1; i <= 10; i += 2) {
     console.log(i);
 }
