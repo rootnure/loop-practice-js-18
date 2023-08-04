3ad6b7e224c927f285c427872ff0e175b138d566
rootnure <nur.diu.2791@gmail.com>
2023-08-04 12:53:44 +0600

    While loop to print numbers and even-odd from range

---
+++ even-odd.js
@@ -0,0 +1,13 @@
+// target: 0 to 20 all even numbers
+var number = 0;
+while(number <= 20) {
+    console.log(number, 'is even');
+    number += 2;
+}
+
+// target: 0 to 10 all odd numbers
+var number = 1;
+while(number <= 20) {
+    console.log(number, 'is odd');
+    number += 2;
+}
---
+++ numbers.js
@@ -0,0 +1,6 @@
+// Target: Display 1 to 10 using while loop
+var number = 1;
+while(number <= 10) {
+    console.log(number);
+    number++;
+}
