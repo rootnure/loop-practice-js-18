7f654124cda98d383f55669807fe57fee647d1d5
rootnure <nur.diu.2791@gmail.com>
2023-08-04 16:41:16 +0600

    Loop in a reverse (decreasing) way

---
+++ reverse.js
@@ -0,0 +1,25 @@
+/* 
+var num = 1;
+while(num <= 10){
+    console.log(num);
+    num++;
+}
+ */
+
+// while reverse
+/* 
+var num = 10;
+while(num >= 1) {
+    console.log(num);
+    num--;
+}
+ */
+/* 
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+ */
+
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
