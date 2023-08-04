77c8a612383ca3d8a1a029be838356bce9792e93
rootnure <nur.diu.2791@gmail.com>
2023-08-04 15:33:07 +0600

    For loop

---
+++ for.js
@@ -0,0 +1,34 @@
+/* 
+var roastGiven = 0;
+while(roastGiven < 7){
+    console.log(roastGiven, 'Roast den Please!!!');
+    roastGiven++;
+}
+ */
+
+// for loop
+/* 
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log(roastGiven, 'Roast den Please!!!');
+}
+ */
+
+// simple version of for loop
+/* 
+console.log('simple for loop');
+for (var i = 0; i < 7; i++) {
+    console.log(i, 'Roast den Please');
+}
+ */
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+for (var i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+for (var i = 1; i <= 10; i += 2) {
+    console.log(i);
+}
