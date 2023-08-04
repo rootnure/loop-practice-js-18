cd609aec6440bcab5638107e02450a392ff61ac6
rootnure <nur.diu.2791@gmail.com>
2023-08-04 12:27:51 +0600

    While loop

---
+++ while.js
@@ -0,0 +1,18 @@
+/* if(condition == true){
+
+} */
+
+
+/*
+1. Loop variable
+2. condition inside while
+3. Loop body
+4. Change the loop variable
+*/
+
+var roastGiven = 0;
+while(roastGiven < 7){
+    console.log(roastGiven);
+    console.log('Roast den please!!!');
+    roastGiven++;
+}
