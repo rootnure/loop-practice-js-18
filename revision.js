945c92b48778c3ec4149a56bc0bb0e7e3602f0d7
rootnure <nur.diu.2791@gmail.com>
2023-08-04 12:11:16 +0600

    Recap

---
+++ revision.js
@@ -0,0 +1,28 @@
+// string type variable
+var bottleContain = 'water';
+var bottleColor = 'Silver';
+// number type variable
+var myAge = 19;
+var bottleCapacityInLiter = 1;
+// boolean type variable
+var isSealedBottle = true;
+var isWaterHot = false;
+
+// array
+var fruits = ['apple', 'banana', 'orange', 'watermelon'];
+var secondItem = fruits[1]
+fruits[3] = 'Pineapple';
+var bananaIndex = fruits.indexOf('banana');
+
+// push pop
+
+// conditionals
+if(fruits[2] === 'Orange'){
+
+}
+else if(fruits.length >= 3){
+
+}
+else {
+    
+}
